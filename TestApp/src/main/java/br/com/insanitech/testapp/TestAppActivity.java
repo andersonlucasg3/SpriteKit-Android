ebd5f1e107a2ce379b54e4754b3fd3d7001a6f80
Anderson Ramos <aramos@gazeus.com>
2016-01-13 19:17:15 -0200

    Added logic to animate the texture mapping changing to test the mapping;

--- TestApp/src/main/java/br/com/insanitech/testapp/TestAppActivity.java
+++ TestApp/src/main/java/br/com/insanitech/testapp/TestAppActivity.java
@@ -2,13 +2,21 @@ package br.com.insanitech.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import br.com.insanitech.spritekit.*;
-import br.com.insanitech.spritekit.logger.Logger;
-import br.com.insanitech.spritekit.opengl.model.GLUtils;
 
 import java.util.Arrays;
+
+import br.com.insanitech.spritekit.SKAction;
+import br.com.insanitech.spritekit.SKColor;
+import br.com.insanitech.spritekit.SKNode;
+import br.com.insanitech.spritekit.SKRect;
+import br.com.insanitech.spritekit.SKScene;
+import br.com.insanitech.spritekit.SKSize;
+import br.com.insanitech.spritekit.SKSpriteNode;
+import br.com.insanitech.spritekit.SKTexture;
+import br.com.insanitech.spritekit.SKView;
 
 /**
  * Created by anderson on 24/06/15.
@@ -40,7 +48,7 @@ public class TestAppActivity extends Activity implements View.OnTouchListener {
 
             texture = new SKTexture(this, R.drawable.card_deck);
 
-            SKSpriteNode swordA = SKSpriteNode.spriteNode(SKColor.redColor(),
+            SKSpriteNode swordA = SKSpriteNode.spriteNode(SKColor.whiteColor(),
                     new SKSize(texture.getSize().getWidth() / 14.0f, texture.getSize().getHeight() / 4.0f));
             swordA.setTexture(new SKTexture(new SKRect(0.0f, 0.0f, 1.0f / 14.0f, 1.0f / 4.0f), texture));
             swordA.setColorBlendFactor(0.5f);
@@ -56,7 +64,7 @@ public class TestAppActivity extends Activity implements View.OnTouchListener {
 
             view.setOnTouchListener(this);
 
-//            rotate(nodeParent);
+            animateMovingCards(swordA);
         }
     }
 
@@ -78,8 +86,37 @@ public class TestAppActivity extends Activity implements View.OnTouchListener {
 
                 return true;
 
-            default:break;
+            default:
+                break;
         }
         return false;
+    }
+
+    private void animateMovingCards(final SKSpriteNode sprite) {
+        float spriteCoordX = sprite.getTexture().textureRect().getX();
+        float spriteCoordY = sprite.getTexture().textureRect().getY();
+
+        spriteCoordX += 1.0f / 14.0f;
+
+        if (spriteCoordX >= 1.0f) {
+            spriteCoordX = 0.0f;
+            spriteCoordY += 1.0f / 4.0f;
+        }
+
+        if (spriteCoordY >= 1.0f) {
+            spriteCoordX = 0.0f;
+            spriteCoordY = 0.0f;
+        }
+
+        Log.d("TEX RECT", "textureRect: " + sprite.getTexture().textureRect());
+
+        sprite.runAction(SKAction.sequence(Arrays.asList(SKAction.waitFor(1000),
+                SKAction.setTexture(new SKTexture(new SKRect(spriteCoordX, spriteCoordY, spriteCoordX + 1.0f / 14.0f, spriteCoordY + 1.0f / 4.0f), texture)),
+                SKAction.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        animateMovingCards(sprite);
+                    }
+                }))));
     }
 }
