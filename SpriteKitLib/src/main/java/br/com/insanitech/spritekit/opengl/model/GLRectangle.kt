c5477d5bb7e5fa7c6847ae5834cdace29ce352d6
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2017-10-03 22:44:26 -0300

    Refactored GLRectangle class, modified visibility;

--- SpriteKitLib/src/main/java/br/com/insanitech/spritekit/opengl/model/GLRectangle.kt
+++ SpriteKitLib/src/main/java/br/com/insanitech/spritekit/opengl/model/GLRectangle.kt
@@ -3,13 +3,10 @@ package br.com.insanitech.spritekit.opengl.model
 /**
  * Created by anderson on 7/3/15.
  */
-class GLRectangle : GLGeometry() {
-    override lateinit var vertices: FloatArray
-        get set
-
+internal class GLRectangle : GLGeometry() {
     init {
-        vertices = floatArrayOf(0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f)
-        componentsPerVertice = 2
+        this.vertices = floatArrayOf(0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f)
+        this.componentsPerVertices = 2
         generateVertex()
     }
 }
