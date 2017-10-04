fcf36394f05489857e23d5239a64f575ea0c8b3b
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2017-10-03 22:34:54 -0300

    Refactored GLColor class, modified visibility of class and properties;

--- SpriteKitLib/src/main/java/br/com/insanitech/spritekit/opengl/model/GLColor.kt
+++ SpriteKitLib/src/main/java/br/com/insanitech/spritekit/opengl/model/GLColor.kt
@@ -9,59 +9,44 @@ import br.com.insanitech.spritekit.core.ValueAssign
 /**
  * Created by anderson on 6/30/15.
  */
-open class GLColor() : ValueAssign<GLColor> {
-    internal val buffer: FloatBuffer
+internal class GLColor() : ValueAssign<GLColor> {
+    internal val buffer = FloatBuffer.allocate(4)
 
-    var r: Float = 1f
-        set(value) { field = value; updateBuffer() }
+    var r: Float
+        get() = this.get(0)
+        set(value) { this.set(0, value) }
 
-    var g: Float = 1f
-        set(value) { field = value; updateBuffer() }
+    var g: Float
+        get() = this.get(1)
+        set(value) { this.set(1, value) }
 
-    var b: Float = 1f
-        set(value) { field = value; updateBuffer() }
+    var b: Float
+        get() = this.get(2)
+        set(value) { this.set(2, value) }
 
-    var a: Float = 1f
-        set(value) { field = value; updateBuffer() }
-
-    init {
-        val q = ByteBuffer.allocateDirect(16 * 4)
-        q.order(ByteOrder.nativeOrder())
-        buffer = q.asFloatBuffer()
-        updateBuffer()
-    }
+    var a: Float
+        get() = this.get(3)
+        set(value) { this.set(3, value) }
 
     constructor(r: Float, g: Float, b: Float, a: Float) : this() {
         this.r = r
-        this.b = b
         this.g = g
+        this.b = b
         this.a = a
-        this.updateBuffer()
     }
 
-    private fun updateBuffer() {
-        buffer.clear()
-        for (i in 0..3) {
-            buffer.put(r)
-            buffer.put(g)
-            buffer.put(b)
-            buffer.put(a)
-        }
-        buffer.position(0)
-    }
+    private fun get(index: Int) : Float = this.buffer.get(index)
+    private fun set(index: Int, value: Float) = this.buffer.put(index, value)
 
     override fun assignByValue(other: GLColor) {
-        r = other.r
-        g = other.g
-        b = other.b
-        a = other.a
-        updateBuffer()
+        this.r = other.r
+        this.g = other.g
+        this.b = other.b
+        this.a = other.a
     }
 
     companion object {
-
+        fun rgb(r: Float, g: Float, b: Float): GLColor = rgba(r, g, b, 1.0f)
         fun rgba(r: Float, g: Float, b: Float, a: Float): GLColor = GLColor(r, g, b, a)
-
-        fun rgb(r: Float, g: Float, b: Float): GLColor = GLColor(r, g, b, 1.0f)
     }
 }
